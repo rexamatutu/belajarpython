b20717bd2c73075da7c3b9617af123bea2565ff8
rexamatutu <a_rexa@smait-nfbsbogor.sch.id>
2022-10-09 14:28:27 +0700

    Operator,Logical,Comparison
    Operator,Logical,Comparison

---
+++ Comparison.py
@@ -0,0 +1,9 @@
+a = int(input("Masukan Bilangan Satu :"))
+b = int(input("Masukan Bilangan Dua :"))
+
+print(a < b) #False
+print(a <= b) #False
+print(a > b) #True
+print(a >= b) #True
+print(a == b) #False
+print(a != b) #False
---
+++ Operator.py
@@ -0,0 +1,8 @@
+bil_1 = int(input("Masukan Bilangan Satu :"))
+bil_2 = int(input("Masukan Bilangan dua :"))
+
+# addition
+hasil_tambah = bil_1 + bil_2
+print("Hasil Penambahan = " ,hasil_tambah)
+
+# modulus adalah sisa hasil bagi (%)
