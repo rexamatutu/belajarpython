281eff88ffb89b205c01bf3ed661bf574f176738
rexamatutu <a_rexa@smait-nfbsbogor.sch.id>
2022-10-23 14:26:30 +0700

    Create If.py

---
+++ If.py
@@ -0,0 +1,10 @@
+num = int(input('Enter any number: '))
+if (num % 5 == 1):
+    print(f'Given number {num} is divisible by 5')
+    print('BENAR')
+print('Ini Akan di cetak terus')
+print(f'{num % 5}')
+
+
+if (5 > 5 and 5 < 8):
+    print('Hallo')
