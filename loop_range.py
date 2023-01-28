8f29b715dcfa1f11ade2337e5ea966257e1b2fdc
rexamatutu <a_rexa@smait-nfbsbogor.sch.id>
2023-01-28 15:32:30 +0700

    loop_range dan while

---
+++ loop_range.py
@@ -0,0 +1,22 @@
+#for i in range(5):
+ #   print(i)
+
+# loop dimulai dari 1 dan berhanti sebelum 10
+#for i in range(1, 10):
+#    print(i)
+
+# loop dimulai dari 1 dan berhanti sebelum 10 dengan kelipatan 2
+#for i in range(2, 10, 2):
+#   print(i)
+
+#fruits = {'apple','orange','banana'}
+#for i in range(len(fruits)):
+#    print(i, fruits[i])
+
+
+for number in range(1, 100):
+    if number % 2 == 0:
+        print(f"{number} Genap")
+    else:
+        print(f"{number} Ganjil") 
+
